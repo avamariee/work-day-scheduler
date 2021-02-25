e78157d85e13619a3bd06718152ead34b95d1fdb
Ava Bullock <ava.m.bullock@gmail.com>
2021-02-24 19:49:41 -0700

    converted moment to jquery and finished stylings on first page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
+// variables to select each hour block
+
+var nineAm = document.getElementById("hour-9");
+var tenAm = document.getElementById("hour-10");
+var elevenAm = document.getElementById("hour-11");
+var twelvePm = document.getElementById("hour-12");
+var onePm = document.getElementById("hour-1");
+var twoPm = document.getElementById("hour-2");
+var threePm = document.getElementById("hour-3");
+var fourPm = document.getElementById("hour-4");
+var fivePm = document.getElementById("hour-5");
+
+// code to display the current day at the top of the page
+
 function dayDisplay() {
 
     console.log("Test.")
 
-    var displayDay = document.createElement("h2")
-    displayDay.classList.add("lead")
-    displayDay.innerText = "Today is " + moment().format('LL') + ".";
-    document.getElementById("currentDay").appendChild(displayDay)
+    var displayDay = $("<h2>")
+    displayDay.addClass("lead")
+    displayDay.text("Today is " + moment().format('LL') + ".");
+    $("#currentDay").append(displayDay)
 
 }
 
-dayDisplay();
+$(document).ready(function(){
+    dayDisplay();
+})
+
+
+
+
